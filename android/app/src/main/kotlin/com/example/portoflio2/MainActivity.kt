e5eb1945f082921964ddfb7c6c2c47f3174852bd
DarkNEEs <mhany8285@gmail.com>
2023-03-09 08:23:59 +0200

    change name and image

---
+++ android/app/src/main/kotlin/com/example/portoflio2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portoflio2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/icons.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+//*Menu Items
+mixin MenuIcons {
+  static const IconData homeIcon = Icons.add;
+  static const IconData experienceIcon = Icons.add;
+  static const IconData aboutIcon = Icons.add;
+  static const IconData projectIcon = Icons.add;
+  static const IconData contactIcon = Icons.add;
+  static const IconData menuIcon = Icons.add;
+  static const IconData sunIcon = Icons.add;
+  static const IconData moonIcon = Icons.add;
+}
+
+mixin ProjectIcons {
+  static const IconData githubIcon = Icons.add;
+  static const IconData playstoreIcon = Icons.add;
+  static const IconData folderIcon = Icons.add;
+  static const IconData websiteIcon =Icons.add;
+}
+
+mixin ContactIcons {
+  static const IconData githubIcon = Icons.add;
+  static const IconData twitterIcon = Icons.add;
+  static const IconData linkedinIcon =Icons.add;
+  static const IconData instagramIcon = Icons.add;
+  static const IconData facebookIcon = Icons.add;
+  static const IconData telegramIcon = Icons.add;
+}
+
+mixin FormIcon {
+  static const IconData nameIcon = Icons.add;
+  static const IconData messageIcon = Icons.add;
+}
+mixin ExperienceIcon {
+  static const IconData arrowIcon =Icons.add;
+  static const IconData timeIcon = Icons.add;
+}
+mixin ToastIcons {
+  static const IconData successIcon =Icons.add;
+}
+
+mixin TechnologiesIcon {
+  static const IconData reactJsIcon =Icons.add;
+  static const IconData javaIcon = Icons.add;
+  static const IconData pythonIcon = Icons.add;
+  static const IconData githubIcon = Icons.add;
+}
+
+mixin SkillDisplayIcons {
+  static const IconData appIcon = Icons.add;
+  static const IconData websiteIcon = Icons.add;
+  static const IconData mlIcon = Icons.add;
+  static const IconData databaseIcon = Icons.add;
+}
